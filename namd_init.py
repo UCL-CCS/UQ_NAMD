a6883dfb84511bcd2068d0d33e797e1da9529eac
Maxime Vassaux <di36yax3@login04.sng.lrz.de>
2020-07-23 17:06:22 +0200

    fixing indendantion issues in namd_init.py

--- namd_init.py
+++ namd_init.py
@@ -18,8 +18,9 @@ class SimEncoder(uq.encoders.JinjaEncoder, encoder_name='SimEncoder'):
 
         simulation_time = 10**params["simulation_time_power"]
         params["n_steps"] = int( round( simulation_time / params["timestep"] ) )
-        params["dcd_freq"] = min(int(params["n_steps"]/48), 5000) # 48 as the number of cores on a node (see anaysis.sh)
-	super().encode(params, target_dir, fixtures)
+        # 48 as the number of cores on a node (see anaysis.sh)
+        params["dcd_freq"] = min(int(params["n_steps"]/48), 5000)
+        super().encode(params, target_dir, fixtures)
 
 class Eq1Encoder(uq.encoders.JinjaEncoder, encoder_name='Eq1Encoder'):
     def encode(self, params={}, target_dir='', fixtures=None):
