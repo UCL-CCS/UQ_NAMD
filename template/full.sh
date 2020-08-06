c8fa8521e857f8d60df9acd03deb767877fa1ab4
Maxime Vassaux <di36yax3@login01.sng.lrz.de>
2020-08-06 15:27:03 +0200

    cleaning full.sh, and automatising job farming nodes count

--- template/full.sh
+++ template/full.sh
@@ -32,7 +32,14 @@ module load amber
 source /lrz/sys/applications/amber/amber18/amber.sh
 module load namd
 
+#n_drugs=2
+#ldrugs="g15 lig0"
 n_drugs=1
+ldrugs="g15"
+n_replicas=3
+
+echo "Running equilibration and simulation on " $((1*$SLURM_JOB_NUM_NODES/$n_drugs)) " nodes or " $((1*$SLURM_NTASKS/$n_drugs)) " cores" 
+echo "Running analysis on " $((1*$SLURM_JOB_NUM_NODES/$n_replicas)) " nodes or " $((1*$SLURM_NTASKS/$n_replicas)) " cores" 
 
 # Path of the UQ_NAMD project
 path_uqnamd=/hppfs/work/pn72qu/di36yax3/tmp/uq_namd2
@@ -41,7 +48,7 @@ path_uqnamd=/hppfs/work/pn72qu/di36yax3/tmp/uq_namd2
 path_template=${path_uqnamd}/template
 
 # Model Builder
-for drug in g15; do
+for drug in $ldrugs; do
     cd $drug/build
     tleap -s -f tleap.in > tleap.log
     awk -f ${path_template}/$drug/build/constraint.awk complex.pdb ${path_template}/$drug/constraint/prot.pdb > ../constraint/cons.pdb
@@ -52,9 +59,9 @@ done
 
 # Equilibration simulation
 for step in {0..2}; do
-    for drug in `ls -d g15`; do
+    for drug in $ldrugs; do
         if [ -s ${drug}/build/complex.prmtop ]; then
-           srun -N 3 -n $((1*$SLURM_NTASKS/$n_drugs)) namd2 +replicas 3 ${drug}/replica-confs/eq$step-replicas.conf &
+           srun -N $((1*$SLURM_JOB_NUM_NODES/$n_drugs)) -n $((1*$SLURM_NTASKS/$n_drugs)) namd2 +replicas 3 ${drug}/replica-confs/eq$step-replicas.conf &
            sleep 5
         fi
     done
@@ -63,9 +70,9 @@ done
 
 # simulation
 for step in {1..1}; do
-    for drug in `ls -d g15`; do
+    for drug in $ldrugs; do
         if [ -s ${drug}/build/complex.prmtop ]; then
-           srun -N 3 -n $((1*$SLURM_NTASKS/$n_drugs)) namd2 +replicas 3 ${drug}/replica-confs/sim$step-replicas.conf &
+           srun -N $((1*$SLURM_JOB_NUM_NODES/$n_drugs)) -n $((1*$SLURM_NTASKS/$n_drugs)) namd2 +replicas 3 ${drug}/replica-confs/sim$step-replicas.conf &
            sleep 5
         fi
     done
@@ -74,19 +81,19 @@ done
 
 # MMPB(GB)SA
 #for drug in "${drugs[@]}"; do
-for drug in g15; do
-    for i in {1..3}; do
+for drug in $ldrugs; do
+    for i in $(seq 1 $n_replicas); do
         cd $drug/fe/mmpbsa/rep$i
 #        srun -N 1 -n 48 MMPBSA.py.MPI -i ../../../../mmpbsa.in -cp ../../build/com.top -rp ../../build/rec.top -lp ../../build/lig.top -y ../../dcd/rep$i.dcd &
-        srun -N 1 -n 48 MMPBSA.py.MPI -i ${path_template}/mmpbsa.in -sp ../../../build/complex.prmtop -cp ../../build/com.top -rp ../../build/rec.top -lp ../../build/lig.top -y ../../../replicas/rep$i/simulation/sim1.dcd
+        srun -N $((1*$SLURM_JOB_NUM_NODES/$n_replicas)) -n $((1*$SLURM_NTASKS/$n_replicas)) MMPBSA.py.MPI -i ${path_template}/mmpbsa.in -sp ../../../build/complex.prmtop -cp ../../build/com.top -rp ../../build/rec.top -lp ../../build/lig.top -y ../../../replicas/rep$i/simulation/sim1.dcd
         sleep 3
         cd ../../../../
     done
 done
 wait
 
-for drug in g15; do
-    for i in {1..3}; do
+for drug in $ldrugs; do
+    for i in $(seq 1 $n_replicas); do
         cd $drug/fe/mmpbsa/rep$i
         srun -n 1 -N 1 xargs -d '\n' -I cmd -P 9 /bin/bash -c 'cmd'  <<EOF &
 cat _MMPBSA_complex_gb.mdout.{0..47} > _MMPBSA_complex_gb.mdout.all
@@ -106,8 +113,8 @@ EOF
 done
 wait
 
-for drug in g15; do
-    for i in {1..3}; do
+for drug in $ldrugs; do
+    for i in $(seq 1 $n_replicas); do
         cd $drug/fe/mmpbsa/rep$i
         rm _MMPBSA_*.{0..47} reference.frc *.pdb *.inpcrd *.mdin* *.out
         cd ../../../../
@@ -115,8 +122,8 @@ for drug in g15; do
 done
 
 echo "drug,replica,binding_energy_avg,binding_energy_stdev" > output.csv
-for drug in g15; do
-    for i in {1..3}; do
+for drug in $ldrugs; do
+    for i in $(seq 1 $n_replicas); do
         cd $drug/fe/mmpbsa/rep$i
         tmp_str=$(awk '{if(index($0, "DELTA TOTAL")> 0) {count++}; if(count>1) { print $3 "," $4; count=0}} ' ./FINAL_RESULTS_MMPBSA.dat)
         cd ../../../../
